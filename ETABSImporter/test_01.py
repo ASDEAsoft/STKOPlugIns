721ad17bf86b1423562559059be0921a6d1a96f6
Massimo Petracca <mazuma88@hotmail.com>
2025-04-07 10:00:52 +0200

    Update test_01.py

--- ETABSImporter/test_01.py
+++ ETABSImporter/test_01.py
@@ -1,3 +1,19 @@
+import sys
+sys.path.append('C:/Develop/Utility/STKO-install/data/win64')
+
 from ETABSImporter.parser import parser
-p = parser('Model_Info.txt')
-p.doc.plot()
+#p = parser('Examples/Ex00/Model_Info.txt')
+p = parser('Examples/Ex01/STKO_IMPORT_40_Story_Rc_Core_Wall_Bldng_e4.3.txt')
+
+
+#p.doc.split_area_members()
+print(p.doc)
+
+p.doc.plot(vertices=False, frames=True, areas=True)
+
+'''
+Assumptions
+areas are generic flat polygons
+- they can be split in multiple lines
+- holes use another command *AREA_OPENINGS_CONNECTIVITY
+'''
